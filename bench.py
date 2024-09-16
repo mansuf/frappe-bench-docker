29c9bed3f29d928a8e725cd6b44708619f19a7b4
Rahman Yusuf <mansuf@mansuf.link>
2024-09-17 00:31:41 +0700

    FINALLY ITS DONE

--- bench.py
+++ bench.py
@@ -1,6 +1,12 @@
 import subprocess
 import sys
 import shlex
+import os
+
+try:
+    bench_dir = os.environ["frappe_bench_dir"]
+except KeyError:
+    raise Exception(".env file is not initialized")
 
 def main():
     # Start mariadb
@@ -10,6 +16,8 @@ def main():
     subprocess.run(["service", "redis-server", "start"])
 
     cmdargs = " ".join(map(shlex.quote, sys.argv[1:]))
+
+    os.chdir(bench_dir)
 
     subprocess.run(["su", "frappe-bench", "-c", "NVM_DIR=/usr/local/nvm; "
                                                 "NODE_VERSION=v20.17.0; "
